c1f0d81774d0cd99dab89250ea36c9133c105cb1
Abijith <jithu12abi@gmail.com>
2020-09-02 20:00:18 +0530

    Made an initial Script to take a URL as input and get a corresponding long url and insert into the table

---
+++ index.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import sqlite3
+import string
+import random
+
+dom = 'short.cut/'
+length = 8
+letters = string.ascii_letters
+
+conn = sqlite3.connect('main.db')
+cur = conn.cursor()
+
+def init():
+    cur.executescript('CREATE TABLE IF NOT EXISTS url(id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,long varchar(200),short varchar(50));')
+
+def create():
+    short = ''
+    short += dom
+    short += ''.join(random.choice(letters) for i in range(length))     
+    print(short)
+    return short
+
+def main():
+    init()
+    url = input('Enter the URL: ')
+    short = create()
+    cur.execute('INSERT INTO url (long,short) VALUES ( ?, ?) ',( url, short))
+
+if __name__ == "__main__":
+    main()    
+
+    #cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id) 
+    #    VALUES ( ?, ? )''', ( album, artist_id ) )
